dbcd59a289e9506a7bb8b44072b9545ed1215c1b
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-02-01 23:17:24 -0800

    Added fadeOut to wrong answers

--- jQuery-projects/2-trivia-card/main.js
+++ jQuery-projects/2-trivia-card/main.js
@@ -1,5 +1,14 @@
 $(document).ready(() => {
   $('.hint-box').on('click', () => {
     $('.hint').slideToggle(500);
-  })
+  });
+  $('.wrong-answer-one').on('click', () => {
+    $('.wrong-text-one').fadeOut('slow');
+  });
+  $('.wrong-answer-two').on('click', () => {
+    $('.wrong-text-two').fadeOut('fast');
+  });
+  $('.wrong-answer-three').on('click', () => {
+    $('.wrong-text-three').fadeOut('slow');
+  });
 });
