6f8f5994fc5d30bf270c7979028e04a51c3e44f8
Web Intern <web.intern@canucks.com>
2019-02-05 10:13:05 -0800

    Added chained events

--- CodeCademy/8-jQuery-mouse-events/main.js
+++ CodeCademy/8-jQuery-mouse-events/main.js
@@ -2,8 +2,18 @@ $(document).ready(() => {
   $('.login-button').on('click', () => {
     $('.login-form').show();
   });
+
   $('.menu-button').on('mouseenter', () => {
-    $('.nav-menu').show();
+    $('.nav-menu').show()
   })
 
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+  })
+  // Chaining events `mouseenter` and `mouseleave`
+  $('.product-photo').on('mouseenter', () => {
+    $('.product-photo').addClass('photo-active');
+  }).on('mouseleave', () => {
+    $('.product-photo').removeClass('photo-active');
+  });
 });
