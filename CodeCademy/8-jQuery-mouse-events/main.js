c10e84c4301b85049dddc89db365bfc52c430280
Web Intern <web.intern@canucks.com>
2019-02-05 10:00:55 -0800

    Added nav menu

--- CodeCademy/8-jQuery-mouse-events/main.js
+++ CodeCademy/8-jQuery-mouse-events/main.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
   $('.login-button').on('click', () => {
     $('.login-form').show();
+  });
+  $('.menu-button').on('mouseenter', () => {
+    $('.nav-menu').show();
   })
-
 
 });
