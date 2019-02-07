97aa8602dbe70dce7757cd205ff1906cc2c58922
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-02-06 20:27:31 -0800

    Added mouse and keyboard events

--- CodeCademy/8-jQuery-mouse-events/main.js
+++ CodeCademy/8-jQuery-mouse-events/main.js
@@ -1,8 +1,40 @@
 $(document).ready(() => {
-  $('.login-button').on('____', () => {
+  $('.login-button').on('dblclick', () => {
     $('.login-form').show();
+  });
+
+  $('.menu-button').on('mousemove', () => {
+    $('.nav-menu').hide()
   })
 
+  $('.login-button').on('keydown', () => {
+    $('.login-form').show();
+  });
+
+  $('.login-button').on('keypress', () => {
+    $('.login-form').show();
+  });
+
+  $('.login-button').on('keyup', () => {
+    $('.login-form').hide();
+  });
+
+  $('.login-button').on('click', () => {
+    $('.login-form').show();
+  });
+
+  $('.menu-button').on('mouseenter', () => {
+    $('.nav-menu').show()
+  })
+
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+  })
+
+  $('.product-photo').on('mouseenter', event => {
+    $(event.currentTarget).addClass('photo-active')
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass('photo-active')
+  })
 
 });
-
