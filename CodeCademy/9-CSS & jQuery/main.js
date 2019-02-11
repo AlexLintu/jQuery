c3cc771190f8b53d56ca8a0195a363cd9747c505
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-02-10 20:13:18 -0800

    Added CSS method

--- CodeCademy/9-CSS & jQuery/main.js
+++ CodeCademy/9-CSS & jQuery/main.js
@@ -6,10 +6,12 @@ $(document).ready(() => {
 
   $('.menu-button').on('mouseenter', () => {
     $('.nav-menu').show();
+    $('.menu-button').css('color', '#C3FF00');
   })
 
   $('.nav-menu').on('mouseleave', () => {
     $('.nav-menu').hide();
+    $('.menu-button').css('color', '#EFEFEF');
   })
 
 }); 
