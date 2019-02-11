efbb8e8519d75ec223d1946255761cf7c24f2a4a
Web Intern <web.intern@canucks.com>
2019-02-11 14:26:36 -0800

    Added css as an object

--- CodeCademy/9-CSS & jQuery/main.js
+++ CodeCademy/9-CSS & jQuery/main.js
@@ -6,12 +6,17 @@ $(document).ready(() => {
 
   $('.menu-button').on('mouseenter', () => {
     $('.nav-menu').show();
-    $('.menu-button').css('color', '#C3FF00');
+    $('.menu-button').css({
+      color: '#C3FF00',
+      backgroundColor: '#535353'
+    });
   })
 
   $('.nav-menu').on('mouseleave', () => {
     $('.nav-menu').hide();
-    $('.menu-button').css('color', '#EFEFEF');
+    $('.menu-button').css({
+      color: '#EFEFEF',
+      backgroundColor: '#303030'
+    });
   })
-
-}); 
+});
