e9184bd4bc4f3fbfe20827bb7c13874ea3b5cf70
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-02-16 22:58:20 -0500

    Updated fontFamily

--- jQuery-projects/4-web-safe-font-preview/main.js
+++ jQuery-projects/4-web-safe-font-preview/main.js
@@ -1,5 +1,13 @@
 $(document).ready(() => {
   $('#text').on('keyup', event => {
+    // Updating preview box
     $('.preview').html($(event.currentTarget).val());
+    // Updating font within preview box
+    $('#font').on('change', event => {
+      $('.preview').css({
+        fontFamily: $(event.currentTarget).val()
+      })
+    })
   });
+
 })
